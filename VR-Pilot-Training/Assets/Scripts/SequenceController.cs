c2828f0c029bca1aea2f8f03a352721c99bf2abb
Aristeus <jornjellema@hotmail.com>
2021-12-05 12:30:42 +0100

    Made a small start for the next part we have to do

---
+++ VR-Pilot-Training/Assets/Scripts/SequenceController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SequenceController : MonoBehaviour
+{
+    /*Create costum sequence. 
+     * (Button 1 on pos 3, button 2 on pos 1, button 3 on pos 4)
+     * Knob rotator, leverscript, and button scripts trigger public voids in this script.
+     * On their side we can determine which position is correct by activating a public void here.
+     * This needs a number variable number of Unity events " public List<UnityEvent> sequence; "
+     * If all are correct it's green lights.
+    */
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
