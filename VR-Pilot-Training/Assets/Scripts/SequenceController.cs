fde5545c5cb6eef9d3e658b013b7ebd0b30a1f86
Aristeus <jornjellema@hotmail.com>
2021-12-20 18:23:42 +0100

    Added a "complete" and a way to see your progress.
    Have to change it up a bit if I want the user to do it in order.

--- VR-Pilot-Training/Assets/Scripts/SequenceController.cs
+++ VR-Pilot-Training/Assets/Scripts/SequenceController.cs
@@ -1,34 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.Events;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class SequenceController : MonoBehaviour
 {
-    /*Create costum sequence. 
-     * (Button 1 on pos 3, button 2 on pos 1, button 3 on pos 4)
-     * Knob rotator, leverscript, and button scripts trigger public voids in this script.
-     * On their side we can determine which position is correct by activating a public void here.
-     * 
-     * Every button has a seperate function they can call
-     * If all buttons are done correct in sequence level iscomplete
-     * Problems: 
-     * - Has to be done in sequence, if it is wrong it has to reset.
-     * - User may not be able to keep sending the correct awnser from the same button, so it has to be done with a true & false system instead of a += 1 system.
-     * - Some buttons could already be on the right position
-     * 
-     * ========================================================================================================================
-     * Bit spagetti, but should work as long as all buttons send their current position as soon as they start to this script. 
-     * Especcialy if those positions are wrong.
-     * ========================================================================================================================
+     /* ===========================================
+     * Bit spagetti, but it works. 
+     * ============================================
     */
 
-    //public List<UnityEvent> sequence;
-    //[SerializeField] [Range(1, 10)] int stepsToComplete = 4;
-    //int stepsCorrect = 0;
     int timer = 0;
-    bool scoreDecrease = false;
     int score = 0;
+    bool scoreDecrease = false;
+    bool completed = false;
+    public string sceneName;
+    public GameObject myCanvas;
     bool one = true;
     bool two = true;
     bool three = true;
@@ -49,7 +37,8 @@ public class SequenceController : MonoBehaviour
             scoreDecrease = true;
         }
 
-        if (one == true &&
+        if (completed == false && 
+            one == true &&
             two == true &&
             three == true &&
             four == true &&
@@ -60,21 +49,19 @@ public class SequenceController : MonoBehaviour
             nine == true &&
             ten == true)
         {
-            levelComplete();
+            completed = true;
+            StartCoroutine(LevelComplete());
         }
-
     }
 
     //Still looking for a prettier way to achive the same result
     public void stepOne()
     {
         one = true;
-        Debug.Log("Step 1 correct");
     }
     public void stepOneWrong()
     {
         one = false;
-        Debug.Log("Step 1 WRONG!");
         if (scoreDecrease == true)
         {
             score -= 1;
@@ -83,13 +70,14 @@ public class SequenceController : MonoBehaviour
 
     public void stepTwo()
     {
-        two = true;
-        Debug.Log("Step 2 correct");
+        //if (one == true)
+        //{
+            two = true;
+        //}
     }
     public void stepTwoWrong()
     {
         two = false;
-        Debug.Log("Step 2 WRONG!");
         if (scoreDecrease == true)
         {
             score -= 1;
@@ -98,13 +86,14 @@ public class SequenceController : MonoBehaviour
 
     public void stepThree()
     {
-        three = true;
-        Debug.Log("Step 3 correct");
+        //if (two == true)
+        //{
+            three = true;
+        //}
     }
     public void stepThreeWrong()
     {
         three = false;
-        Debug.Log("Step 3 WRONG!");
         if (scoreDecrease == true)
         {
             score -= 1;
@@ -113,13 +102,14 @@ public class SequenceController : MonoBehaviour
 
     public void stepFour()
     {
-        four = true;
-        Debug.Log("Step 4 correct");
+        //if (three == true)
+        //{
+            four = true;
+        //}
     }
     public void stepFourWrong()
     {
         four = false;
-        Debug.Log("Step 4 WRONG!");
         if (scoreDecrease == true)
         {
             score -= 1;
@@ -129,12 +119,10 @@ public class SequenceController : MonoBehaviour
     public void stepFive()
     {
         five = true;
-        Debug.Log("Step 5 correct");
     }
     public void stepFiveWrong()
     {
         five = false;
-        Debug.Log("Step 5 WRONG!");
         if (scoreDecrease == true)
         {
             score -= 1;
@@ -144,12 +132,10 @@ public class SequenceController : MonoBehaviour
     public void stepSix()
     {
         six = true;
-        Debug.Log("Step 6 correct");
     }
     public void stepSixWrong()
     {
         six = false;
-        Debug.Log("Step 6 WRONG!");
         if (scoreDecrease == true)
         {
             score -= 1;
@@ -208,25 +194,18 @@ public class SequenceController : MonoBehaviour
         }
     }
 
-
+    /*
     public void levelComplete()
     {
         Debug.Log("Level complete");
         Debug.Log("Minus points: " + score);
     }
-    /*
-    public void resetSquence()
-    {
-        one = false;
-        two = false;
-        three = false;
-        four = false;
-        five = false;
-        six = false;
-        seven = false;
-        eight = false;
-        nine = false;
-        ten = false;
-    }
     */
+    IEnumerator LevelComplete()
+    {
+        myCanvas.SetActive(true);
+        Debug.Log("Minus points: " + score);
+        yield return new WaitForSeconds(4);
+        SceneManager.LoadScene(sceneName);
+    }
 }
